fb76990c0cca3b3b31ada121993a642357e6d178
Tri-C Faculty-Staff <Tri-C Faculty-Staff@LT5CG6250Q8R>
2017-06-14 09:39:16 -0900

    made it work now

---
+++ ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        public static string AskQuestion(string word)
+        {
+            Console.WriteLine(word);
+            return Console.ReadLine();
+        }
+        static void Main(string[] args)
+
+        {
+            double yearsExperience = Convert.ToDouble(AskQuestion("How many years experience do you have?"));
+            string degreeAcquired = AskQuestion("What degree did you acquire?");
+
+            double onlineExamScore = Convert.ToDouble(AskQuestion("What was your exam score"));
+
+
+
+        }
+    }
+}
---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        public static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+        public static void testFunction()
+        {
+            List<string> studentList = new List<string>();
+           
+
+            foreach (string student in studentList)
+            {
+                Console.WriteLine(student);
+
+            }
+
+            bool people = true;
+            while (people)
+            {
+                string studentName = AskQuestion("What's your name");
+                double studentScore = Convert.ToDouble(AskQuestion("What's your score"));
+                string anotherOne = AskQuestion("Another one? (y/n)");
+              
+
+            }
+        }
+        public static string didPass(double score)
+        {
+            if (score >= 70)
+            {
+                return "passed";
+            }
+            else
+            {
+                return "failed";
+            }
+        }
+
+        public static void testFunctionNew()
+        {
+            List<string> studentTotalThing = new List<string>();
+            while (true)
+            {
+             
+               
+                string studentFirstName = AskQuestion("What's your first name");
+                string studentLastName = AskQuestion("What's your last name");
+                double studentTestScore = Convert.ToDouble(AskQuestion("What's your score"));
+                string studentCondition = didPass(studentTestScore);
+                studentTotalThing.Add(studentFirstName + " " + studentLastName + " " + studentCondition);
+                foreach(string student in studentTotalThing)
+                {
+                    Console.WriteLine(student);
+                }
+                string anotherOne = AskQuestion("Another one? (y/n)");
+                if (anotherOne == "n")
+                {
+                    break;
+                }
+
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            testFunctionNew();
+        }
+    }
+}
