d91db29e91f617ca091a3e3b525b0f962f710ac8
Tri-C Faculty-Staff <Tri-C Faculty-Staff@LT5CG6250Q8R>
2017-06-14 14:39:15 -0900

    classes and stuff

---
+++ ConsoleApplication15/ConsoleApplication15/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Car
+    {
+        public string make;
+        public string model;
+        string color;
+        int year;
+        float MSRP;
+        int VIN;
+        public float sellCar(string person, float salesPrice)
+        {
+            return this.MSRP / salesPrice * 1000;
+        }
+    }
+}
